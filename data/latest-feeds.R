2eb81ba577b9e1a4756d38338b85f94ca6a5f5f5
mlozanoqf <mlozanoqf@gmail.com>
2026-04-06 02:29:22 -0600

    dates

--- data/latest-feeds.R
+++ data/latest-feeds.R
@@ -1,5 +1,5 @@
-list(updated_at = "2026-03-29 08:35:00", youtube = list(title = "Robo trader simple",
-    link = "https://www.youtube.com/watch?v=sT8B9L-tQ64", date = "",
-    ok = TRUE, source = "live"), substack = list(title = "Respuesta convincente y convicción interna en la IA.",
-    link = "https://ahyaentendi.substack.com/p/respuesta-convincente-y-conviccion",
-    date = "Mar 28, 2026", ok = TRUE, source = "live"))
+list(updated_at = "2026-04-06 08:26:59", youtube = list(title = "Robo trader simple", 
+    link = "https://www.youtube.com/watch?v=sT8B9L-tQ64", date = "", 
+    ok = TRUE, source = "live"), substack = list(title = "La tasa de interés como precio del tiempo.", 
+    link = "https://ahyaentendi.substack.com/p/la-tasa-de-interes-como-precio-del", 
+    date = "Apr 04, 2026", ok = TRUE, source = "live"))
